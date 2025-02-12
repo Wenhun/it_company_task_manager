90aceea1cb6940c85ac60df402979386a9a3bc74
Ilia Terentev <wenhun1@gmail.com>
2025-02-12 14:12:21 +0200

    add 'base.html' template, login and logout pages

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-from task_manager.models import Worker, Task, Team
+from task_manager.models import Task
 
 
 @login_required
@@ -11,17 +11,8 @@ def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
     tasks = Task.objects.filter(assignees=request.user.id).order_by("deadline", "priority")
     project = get_user_model().objects.get(pk=request.user.id).team.project.project_name
-    # num_cars = Car.objects.count()
-    # num_manufacturers = Manufacturer.objects.count()
-    #
-    # num_visits = request.session.get("num_visits", 0)
-    # request.session["num_visits"] = num_visits + 1
 
     context = {
-        # "num_drivers": num_drivers,
-        # "num_cars": num_cars,
-        # "num_manufacturers": num_manufacturers,
-        # "num_visits": num_visits + 1,
         "tasks": tasks,
         "project": project,
     }
