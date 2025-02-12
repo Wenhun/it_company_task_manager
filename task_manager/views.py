04212695abb6bf5bcecd86358caebd8167de25f4
Ilia Terentev <wenhun1@gmail.com>
2025-02-12 14:20:44 +0200

    add the number of tasks to the homepage and welcome text

--- task_manager/views.py
+++ task_manager/views.py
@@ -9,12 +9,17 @@ from task_manager.models import Task
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
-    tasks = Task.objects.filter(assignees=request.user.id).order_by("deadline", "priority")
-    project = get_user_model().objects.get(pk=request.user.id).team.project.project_name
+    current_user_id = request.user.id
+    tasks = Task.objects.prefetch_related("assignees").filter(assignees=current_user_id).order_by("deadline", "priority")
+    project = get_user_model().objects.get(pk=current_user_id).team.project.project_name
+    num_completed_tasks = tasks.filter(is_completed=True).count()
+    num_not_completed_tasks = tasks.filter(is_completed=False).count()
 
     context = {
         "tasks": tasks,
         "project": project,
+        "num_completed_tasks": num_completed_tasks,
+        "num_not_completed_tasks": num_not_completed_tasks,
     }
 
     return render(request, "task_manager/index.html", context=context)
