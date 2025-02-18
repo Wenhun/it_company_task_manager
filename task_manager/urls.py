b79cc80f7b81887649e68fa5fde53a45a98c244d
Ilia Terentev <wenhun1@gmail.com>
2025-02-18 17:12:29 +0200

    add 'task_list' page - view, path and stylization add link to this page on sidebar create pagination

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from task_manager.views import index, TaskDetailView, ProjectDetailView
+from task_manager.views import (index,
+                                TaskDetailView,
+                                TasksListView,
+                                ProjectDetailView)
 
 app_name = "task_manager"
 
@@ -8,4 +11,5 @@ urlpatterns = [
     path("", index, name="index"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("tasks/", TasksListView.as_view(), name="task-list"),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -7,21 +7,23 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.views import generic
 
-from task_manager.models import Task, Project,Team
+from task_manager.models import Task, Project, Team
 
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
     current_user_id = request.user.id
-    tasks = Task.objects.prefetch_related("assignees").filter(
-        assignees=current_user_id)
+    tasks = Task.objects.prefetch_related("assignees").filter(assignees=current_user_id)
     tasks = tasks.order_by("deadline", "priority")
-    project = get_user_model().objects.get(
-        pk=current_user_id).team.project
+    project = get_user_model().objects.get(pk=current_user_id).team.project
     num_completed_tasks = tasks.filter(is_completed=True).count()
     num_not_completed_tasks = tasks.filter(is_completed=False).count()
-    team_workers = get_user_model().objects.filter(team=request.user.team).exclude(id=current_user_id)
+    team_workers = (
+        get_user_model()
+        .objects.filter(team=request.user.team)
+        .exclude(id=current_user_id)
+    )
     current_date = datetime.now().date()
 
     context = {
@@ -30,7 +32,7 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_completed_tasks": num_completed_tasks,
         "num_not_completed_tasks": num_not_completed_tasks,
         "team_workers": team_workers,
-        "current_date": current_date
+        "current_date": current_date,
     }
 
     return render(request, "task_manager/index.html", context=context)
@@ -42,10 +44,24 @@ class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         current_date = datetime.now()
-        task = context['task']
+        task = context["task"]
         days_difference = (current_date.date() - task.deadline).days
-        context['days_difference'] = days_difference
-        context['is_overdue'] = days_difference > 0
+        context["days_difference"] = days_difference
+        context["is_overdue"] = days_difference > 0
         return context
+
+
 class ProjectDetailView(LoginRequiredMixin, generic.DetailView):
     model = Project
+
+
+class TasksListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "task_manager/task_list.html"
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["current_date"] = datetime.now().date()
+        return context
