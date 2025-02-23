8ccd98cbbbf34b92d5183b907eb56af81b66747f
Ilia Terentev <wenhun1@gmail.com>
2025-02-23 14:42:30 +0200

    add search on project_list.html

--- task_manager/views.py
+++ task_manager/views.py
@@ -102,7 +102,20 @@ class ProjectListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["current_date"] = datetime.now().date()
+        project_name = self.request.GET.get("search_field", "")
+        context["search_form"] = SearchForm(
+            initial={"search_field": project_name}, field_name="project_name"
+        )
         return context
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Project.objects.all()
+        form = SearchForm(data=self.request.GET, field_name="project_name")
+        if form.is_valid():
+            return queryset.filter(
+                project_name__icontains=form.cleaned_data["search_field"])
+
+        return queryset
 
 
 class ProjectCreateView(LoginRequiredMixin, generic.CreateView):
@@ -121,7 +134,6 @@ class ProjectUpdateView(LoginRequiredMixin, generic.UpdateView):
 class ProjectDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Project
     success_url = reverse_lazy("task_manager:project-list")
-
 
 
 class WorkerListView(LoginRequiredMixin, generic.ListView):
